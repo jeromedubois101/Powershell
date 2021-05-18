c5baf6ab142a9beba976fa4a257d59f6e3ace3ac
Jerome <jeromedubois101@gmail.com>
2021-05-18 10:18:32 +0200

    Modif

--- Sécurité/Exercice 3.ps1
+++ Sécurité/Exercice 3.ps1
@@ -13,14 +13,36 @@ mkdir "OU=Administration"
 # Ajout des utilisateurs dans le fichier CSV (changer domaine correspondant au préalable)
 
 $users = Import-Csv C:\Users\Administrateur\Desktop\PowerShellSecure.csv -Delimiter ";"
+$change_pass = @{"true"=1;"false"=0}
 foreach($user in $users){
     $fullname = $user.firstname + " " + $user.lastname
-    $lp = Get-Credential -Message "Entrer vos informations d'identification" -UserName $user.username
+    $username = $user.username
+    $lp = Get-Credential -Message "Entrer vos informations d'identification" -UserName $username
     $password = $lp.password
-    if($user.ChangePWlogon -eq "true"){
-        New-ADUser -Name $fullname -GivenName $user.firstname -Surname $user.lastname -SamAccountName $user.username -Department $user.departement -AccountPassword $password -Path $user.ou -Enabled 1 -ChangePasswordAtLogon 1
+    if(Get-ADUser -F {SamAccountName -eq $username})
+    {
+        Write-Warning "Le compte existe déjà"
     }
-    else {
-        New-ADUser -Name $fullname -GivenName $user.firstname -Surname $user.lastname -SamAccountName $user.username -Department $user.departement -AccountPassword $password -Path $user.ou -Enabled 1 
+    else 
+    {
+        try 
+        {
+        New-ADUser `
+            -Name $fullname `
+            -GivenName $user.firstname `
+            -Surname $user.lastname `
+            -SamAccountName $user.username `
+            -DisplayName $fullname  `
+            -Department $user.departement `
+            -AccountPassword $password `
+            -Path $user.ou `
+            -Enabled 1 `
+            -ChangePasswordAtLogon $change_pass[$user.ChangePWlogon]  
+    
+        }
+        catch
+        {
+            Write-Host "Erreur !!!" -ForegroundColor Red
+        }
     }
 }
